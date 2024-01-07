9965c77ace4330845759d060502cfa4059a44dc7
Travis Gunter <travisjgunter@gmail.com>
2024-01-07 01:09:24 -0700

    IDK

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
-from extensions import db
+from .extensions import db
+
 from models import Book
 
 app = Flask(__name__)
